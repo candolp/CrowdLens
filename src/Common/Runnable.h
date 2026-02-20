c9049f8e565c306274357e045aa1270870d23549
candolp <candolpd@gmail.com>
2026-02-20 11:50:00 +0000

    add IRSensor configuration options and enhance class with traffic state tracking
    create `Runnable` interface to mark callback handlers
    create `TrafficEventHanlder` to extend and add threading the `Runnable` class
    
    add `ConfigLoader` implementation and integrate it into the build system

---
+++ src/Common/Runnable.h
@@ -0,0 +1,21 @@
+//
+// Created by CANDO on 21/02/2026.
+//
+
+#ifndef CROWDLENS_RUNNABLE_H
+#define CROWDLENS_RUNNABLE_H
+#include "CrowLenseState.h"
+
+// An interface which can be added
+// to the receiving class. We call it runnable
+// it contains a single method called "run"!
+class Runnable
+{
+public:
+    virtual ~Runnable() = default;
+    virtual void run(TrafficState traffic_state) = 0;
+    virtual void stop(TrafficState traffic_state);
+};
+
+
+#endif //CROWDLENS_RUNNABLE_H
---
+++ src/IRSensor/IRSensor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by CANDO on 21/02/2026.
+//
+
+#include "IRSensor.h"
