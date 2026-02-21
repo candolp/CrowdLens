1a26f95a4cad641a64f163e9944d10bab89ee9fc
candolp <candolpd@gmail.com>
2026-02-21 16:44:56 +0000

    Add IRSensor configuration options and enhance class with traffic state tracking

--- src/IRSensor/IRSensor.h
+++ src/IRSensor/IRSensor.h
@@ -4,12 +4,35 @@
 
 #ifndef CROWDLENS_IRSENSOR_H
 #define CROWDLENS_IRSENSOR_H
+#include "../Common/ConfigLoader.h"
 #include "../Common/CppTimer.h"
+#include "../Common/CppTimerCallbackInterface.h"
 
+enum traffic_status {
+    NO_TRAFFIC,
+    TRAFFIC,
+};
+enum IRSensorType
+{
+    DIGITAL,
+    ANALOG
+};
 
-class IRSensor: public CppTimer
+class IRSensor: public CppTimerCallbackInterface
 {
     public:
+        IRSensor();
+        explicit IRSensor(const ConfigLoader& config);
+    private:
+        traffic_status state = traffic_status::NO_TRAFFIC;
+        const ConfigLoader& config;
+        int lastStateElaspe = 0;
+        int GPIOPin = 0;
+        IRSensorType sensorType = IRSensorType::DIGITAL;
+
+    protected:
+        void timerEvent() override;
+        void stop() override;
 };
 
 
