1fd739c66b45c65fbf828a8bd114569dd50e3236
candolp <34892322+candolp@users.noreply.github.com>
2026-02-25 22:17:44 +0000

    set up project structure and CI workflow
    * setup project. Created all necessary directories and CMake files
    *create a base GitHub workflow for GitHub actions
    
    * add CMake configuration for CrowdLens executable
    
    * add placeholder CMakeLists for test directories

---
+++ src/demo/Main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by candolp on 20/02/2026.
+//
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
